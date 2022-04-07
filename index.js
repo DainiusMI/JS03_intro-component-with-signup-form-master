6a68f7d16e85c3fc0b1cb0190c9e447ad4083a01
Dainius <100964434+DainiusMI@users.noreply.github.com>
2022-04-07 15:08:26 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
-function validateForm() {   
+function validateEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(document.getElementById('email').value.match(mailformat)) {
+        return true;
+    }
+    else {
+        document.getElementById('email').setAttribute('placeholder', 'email@expamle/com');
+        document.getElementById('email').value = ' ';
+        document.getElementById('email').classList.add('input-error');
+        document.getElementById('email-error').classList.add('show-error');
+        return false;
+    }
+}
 
+function validateForm() {
 
     let fistName = document.getElementById('first-name').value;
     let lastName = document.getElementById('last-name').value;
@@ -10,33 +23,29 @@ function validateForm() {
     let formIdList = ['first-name', 'last-name', 'email', 'password'];
     let errorIdList = [ 'name-error', 'last-error', 'email-error', 'password-error'];
 
-    function validateEmail(email) {
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }    
-
     for (i = 0; i < userInfoArray.length; i++) {
+
         if (userInfoArray[i] == "" || userInfoArray[i] == null) {
+            if (i != 2) {
             document.getElementById(formIdList[i]).setAttribute('placeholder', ' ');
             document.getElementById(formIdList[i]).classList.add('input-error');
             document.getElementById(errorIdList[i]).classList.add('show-error');
-            if (i == 2) {
-                if (validateEmail(email)) {
-                   
-                }
-                else {
-                    document.getElementById(formIdList[i]).setAttribute('placeholder', 'email@exanple/com');
-                }
             }
-            }
-            else { 
+            
+        }
+        else { 
             document.getElementById(formIdList[i]).classList.remove('input-error');
             document.getElementById(errorIdList[i]).classList.remove('show-error');
-            }
+        }
     }
-
-
+    validateEmail(email);
 }
+
+
+
+
+
+
 
 
 
